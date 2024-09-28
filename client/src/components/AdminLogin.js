522980d04115e900204dc408e74128c9685a2901
Andreas Vanikiotis <82836796+andreas7van@users.noreply.github.com>
2024-09-28 15:54:09 +0300

    Update AdminLogin.js

--- client/src/components/AdminLogin.js
+++ client/src/components/AdminLogin.js
@@ -23,7 +23,7 @@ const AdminLogin = () => {
         e.preventDefault();
     
         try {
-            const response = await fetch('http://localhost:3001/admin/login', {
+            const response = await fetch('https://astrology-booking-1.onrender.com/admin/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
